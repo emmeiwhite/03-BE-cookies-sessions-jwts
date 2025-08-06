8cc0ad253bf72bcfb40d6f4b9bdba01ad9671bd3
emmeiwhite <emmeiwhite@gmail.com>
2025-08-06 22:51:15 +0530

    5. Initial Route up and running giving life to the serve

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+
 dotenv.config()
 
-import cors from 'cors'
+const app = express()
 
-app.use(cors)
+app.use(cors())
+app.use(express.json())
+app.use(cookieParser())
+
+// 1. Routes
+app.get('/', (req, res) => {
+  res.status(200).json({
+    success: true,
+    data: {
+      users: ['immi', 'jimmi', 'timmi']
+    }
+  })
+})
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log(`listening to PORT ${PORT}`)
+})
