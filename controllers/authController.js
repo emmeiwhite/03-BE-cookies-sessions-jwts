224f877b32e276738ad1efc1c9c8c2e28da81242
emmeiwhite <emmeiwhite@gmail.com>
2025-08-09 21:11:58 +0530

    26. Validate fields & check user already exists!

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,23 @@
 import User from '../models/User.js'
 
 export const signUp = async (req, res) => {
-  console.log(req.body)
-  // Testing
-  const user = await User.create(req.body)
+  // Testing  const user = await User.create(req.body)
+
+  try {
+    const { name, email, password } = req.body
+    // 1. Validate fields
+    if (!name || !email || !password) {
+      return res.status(400).json({ success: false, msg: 'All fields are required' })
+    }
+
+    // 2. Check if user already exists
+    const existingUser = await User.findOne({ email })
+    if (existingUser) {
+      return res.status(400).json({ success: false, msg: 'Email already registered' })
+    }
+  } catch (error) {
+    req.status(500).json()
+  }
 
   res.status(200).json({
     success: true,
